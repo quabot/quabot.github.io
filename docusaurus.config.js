347627b6e2adc8bc011dff669e7ef74ae430a9ac
Joa_sss <quabotgithub@gmail.com>
2025-11-18 23:18:28 +0100

    Added adsense

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,6 +54,14 @@ const config = {
         content: "#4d7aff",
       },
     },
+    {
+      tagName: "script",
+      attributes: {
+        async: true,
+        src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4961763445419391",
+        crossorigin: "anonymous",
+      },
+    },
   ],
 
   scripts: [
