0c5621b8a886e2b39264e95726652e58fa9142a7
Joa_sss <quabotgithub@gmail.com>
2025-11-18 23:23:04 +0100

    Fixed adsense crash

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
     {
       tagName: "script",
       attributes: {
-        async: true,
+        async: "true",
         src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4961763445419391",
         crossorigin: "anonymous",
       },
